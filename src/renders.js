6bfca6e618dd216de76b3f46bdeb9675f1799755
CosmoBoyMe <2014failz2014@gmail.com>
2021-03-31 21:04:06 +0200

    feat: add modal show

---
+++ src/renders.js
@@ -0,0 +1,113 @@
+import i18next from 'i18next';
+
+const renderError = (formState) => {
+  const feedbackElement = document.querySelector('.feedback');
+  const input = document.querySelector('input');
+  input.classList.add('is-invalid');
+  feedbackElement.classList.add('text-danger');
+  feedbackElement.classList.remove('text-success');
+  feedbackElement.textContent = formState.errorMessage;
+};
+
+const renderHeaderAfterLoad = () => {
+  const feedbackElement = document.querySelector('.feedback');
+  const input = document.querySelector('input');
+  input.value = '';
+  input.focus();
+  input.classList.remove('is-invalid');
+  feedbackElement.classList.add('text-success');
+  feedbackElement.classList.remove('text-danger');
+  feedbackElement.textContent = i18next.t('feedback');
+};
+
+const renderFeed = ({ id, feedTitle, feedDescription }) => {
+  const container = document.querySelector('.feeds');
+  if (id < 2) {
+    const h2 = document.createElement('h2');
+    const ul = document.createElement('ul');
+    ul.classList.add('list-group', 'mb-5');
+    h2.textContent = 'Фиды';
+    container.append(h2, ul);
+  }
+  const ul = document.querySelector('.feeds>ul');
+  const li = document.createElement('li');
+  const h3 = document.createElement('h3');
+  const p = document.createElement('p');
+  li.classList.add('list-group-item');
+  h3.textContent = feedTitle;
+  p.textContent = feedDescription;
+  li.append(h3, p);
+  ul.prepend(li);
+};
+
+const createButton = (postId, postTitle, postDescription, postLink, openedPosts) => {
+  const button = document.createElement('button');
+  button.classList.add('btn', 'btn-primary');
+  button.textContent = i18next.t('button');
+  button.setAttribute('type', 'button');
+  button.setAttribute('id', postId);
+  button.setAttribute('data-bs-toggle', 'modal');
+  button.setAttribute('data-bs-target', '#modal');
+  button.addEventListener('click', (event) => {
+    const modalTitle = document.querySelector('.modal-title');
+    const modalBody = document.querySelector('.modal-body');
+    const openLinkBtn = document.querySelector('.full-article');
+    openLinkBtn.href = postLink;
+    modalTitle.textContent = postTitle;
+    modalBody.textContent = postDescription;
+    openedPosts.push({ id: postId });
+    const postLinkElement = event.target.previousElementSibling;
+    postLinkElement.classList.remove('fw-bold');
+    postLinkElement.classList.add('fw-normal');
+  });
+
+  return button;
+};
+
+const isLinkOpened = (postId, openedPosts) => !!openedPosts.find((item) => postId === item.id);
+
+const createLink = (postId, postTitle, postLink, openedPosts) => {
+  const a = document.createElement('a');
+  a.setAttribute('href', postLink);
+  a.setAttribute('target', 'blank');
+  a.textContent = postTitle;
+  if (isLinkOpened(postId, openedPosts)) {
+    a.classList.add('fw-normal');
+  } else {
+    a.classList.add('fw-bold');
+  }
+  return a;
+};
+
+const renderPosts = (state) => {
+  const container = document.querySelector('.posts');
+  container.innerHTML = '';
+
+  const h2 = document.createElement('h2');
+  h2.textContent = 'Посты';
+
+  const ul = document.createElement('ul');
+  ul.classList.add('list-group');
+  const { openedPosts } = state.uiState;
+  state.posts.forEach((post) => {
+    const {
+      postId, postTitle, postDescription, postLink,
+    } = post;
+
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start');
+
+    const a = createLink(postId, postTitle, postLink, openedPosts);
+    const button = createButton(postId, postTitle, postDescription, postLink, openedPosts);
+    li.append(a, button);
+    ul.prepend(li);
+  });
+  container.append(h2, ul);
+};
+
+export {
+  renderError,
+  renderFeed,
+  renderPosts,
+  renderHeaderAfterLoad,
+};
--- src/view.js
+++ src/view.js
@@ -1,58 +1,40 @@
-const renderErrors = (formState) => {
-  const feedbackElement = document.querySelector('.feedback');
-  const input = document.querySelector('input');
-  console.log(formState);
-  if (formState.valid) {
-    input.classList.remove('is-invalid');
-    feedbackElement.classList.add('text-success');
-    feedbackElement.classList.remove('text-danger');
-    feedbackElement.textContent = '';
-  } else {
-    input.classList.add('is-invalid');
-    feedbackElement.classList.add('text-danger');
-    feedbackElement.classList.remove('text-success');
-    feedbackElement.textContent = formState.errorMessage;
+import * as _ from 'lodash';
+import {
+  renderError, renderFeed, renderPosts, renderHeaderAfterLoad,
+} from './renders.js';
+
+const onChange = require('on-change');
+
+const processStateHandler = (processState, state, domElements) => {
+  switch (processState) {
+    case 'processed':
+      domElements.button.setAttribute('disabled', true);
+      break;
+    case 'loaded':
+      renderFeed(_.last(state.feeds));
+      renderPosts(state);
+      renderHeaderAfterLoad();
+      domElements.button.removeAttribute('disabled');
+      break;
+    case 'failed': {
+      renderError(state.form);
+      domElements.button.removeAttribute('disabled');
+      break;
+    }
+    case 'updated':
+      renderPosts(state);
+      break;
+    default:
+      break;
   }
 };
 
-const renderFeed = ({ id, feedTitle, feedDescription }) => {
-  const container = document.querySelector('.feeds');
-  if (id === 1) {
-    const h2 = document.createElement('h2');
-    const ul = document.createElement('ul');
-    ul.classList.add('list-group', 'mb-5');
-    h2.textContent = 'Фиды';
-    container.append(h2, ul);
-  }
-  const ul = document.querySelector('.feeds>ul');
-  const li = document.createElement('li');
-  li.classList.add('list-group-item');
-  const h3 = document.createElement('h3');
-  const p = document.createElement('p');
-  h3.textContent = feedTitle;
-  p.textContent = feedDescription;
-  li.append(h3, p);
-  ul.prepend(li);
-};
-
-const renderPosts = (posts) => {
-  const container = document.querySelector('.posts');
-  container.innerHTML = '';
-  const reversedPosts = posts;
-  const h2 = document.createElement('h2');
-  const ul = document.createElement('ul');
-  ul.classList.add('list-group');
-  h2.textContent = 'Посты';
-  reversedPosts.forEach((post) => {
-    const { postTitle, postlink } = post;
-    const li = document.createElement('li');
-    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start');
-    const a = document.createElement('a');
-    a.setAttribute('href', postlink);
-    a.textContent = postTitle;
-    li.append(a);
-    ul.prepend(li);
+const initView = (state, domElements) => {
+  const watchedState = onChange(state, (path, value) => {
+    if (path === 'processState') {
+      processStateHandler(value, state, domElements);
+    }
   });
-  container.append(h2, ul);
+  return watchedState;
 };
-export { renderErrors, renderFeed, renderPosts };
+export default initView;
