9b79f935dbbd77ac68c99fd2a95845700e4b82f8
CosmoBoyMe <2014failz2014@gmail.com>
2021-03-31 20:56:25 +0200

    add internationalization-framework and text

---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    formErrors: {
+      invalid: 'Ссылка должна быть валидным URL',
+      used: 'RSS уже существует',
+    },
+    button: 'Просмотр',
+    feedback: 'RSS успешно загружен',
+    network: 'Ошибка сети',
+  },
+};
